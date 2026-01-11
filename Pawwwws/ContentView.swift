b7699f723189da4b7a362ff93bbb09fa316dcbef
Bakhytgali <omgwtfwat@gmail.com>
2026-01-11 21:05:13 +0500

    Initial Commit

---
+++ Pawwwws/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pawwwws
+//
+//  Created by Rakhat Bakhytgali on 11.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pawwwws/PawwwwsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawwwwsApp.swift
+//  Pawwwws
+//
+//  Created by Rakhat Bakhytgali on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PawwwwsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
